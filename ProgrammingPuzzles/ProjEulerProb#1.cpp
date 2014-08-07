3b11f6533ec04454898392b39ce353df9b9f2708
lifelongstudent <rakeshgajjar1983@gmail.com>
2014-08-07 12:30:02 +0530

    [Title ] ProjectEuler Problem Solutions : Solution of problem # 1 [Description] Find out sum of number which are in multiple of 3 and 5. There can be N numbers, where as 1<N<10000, value for N is taken from user. Value whose multiple are to be found are already give. Could make a provision to take them from user.

---
+++ ProgrammingPuzzles/ProjEulerProb#1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>           // std::cout
+
+using namespace std;
+
+int main ()
+{
+	int sum =0, multi3 =3, multi5 =5;
+	int totalNumbers = 0;
+
+	cout<<"Total numbers: ";
+	cin>>totalNumbers;
+	
+	for (int i =1; i<totalNumbers ; i++)
+	{
+		//cout<<"i ["<<i<<"]"<<'\n';
+		if((i%multi3)==0)
+		{
+			sum += i;
+		}
+		else if((i%multi5)==0)
+		{
+			sum+=i;
+		}
+	}
+	cout<<"Sum ["<<sum<<"]"<< '\n';
+	return 0;
+}
