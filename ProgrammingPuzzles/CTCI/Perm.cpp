1e006a97481c894e34c4ad43704b8ec4be5ad4e8
lifelongstudent <rakeshgajjar1983@gmail.com>
2017-01-19 23:32:59 +0530

    1. Code cleanup 2. New test case addition

--- ProgrammingPuzzles/CTCI/Perm.cpp
+++ ProgrammingPuzzles/CTCI/Perm.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 #define MAX_ARRAY_SIZE 100
 
-int N, Ans;
-int totalPerms = 0;
+int N;
 int permuteArr[MAX_ARRAY_SIZE] = { 0 };
 int visited[MAX_ARRAY_SIZE] = { 0 };
-
 int permString[MAX_ARRAY_SIZE] = {0};
+
+
 void Reset();
 bool IsValid(int index);
 int Permute();
-int PermuteRec(int array[], int length, int index, int base, int count);
+int PermuteRec(int array[], int length, int index, int count);
 
 void Reset(){
 	for (int i = 0; i < N; i++){
@@ -21,8 +21,6 @@ void Reset(){
 		visited[i] = 0;
 	}
 	N = 0;
-	Ans = 0;
-	totalPerms = 0;
 }
 bool IsValid(int index){
 	if ((index < 0 || index > N) || visited[index]){
@@ -33,34 +31,27 @@ bool IsValid(int index){
 
 int Permute(int arr[], int length){
 
-	for (int i = 0; i < N; i++){
-		PermuteRec(permuteArr, length, i, i,0);
-	}
+	for (int i = 0; i < N; i++){  PermuteRec(permuteArr, length, i,0); 	}
 	return 0;
 }
 
-int PermuteRec(int array[], int length, int index, int base, int count){
+int PermuteRec(int array[], int length, int index,int count){
 
 	if (!IsValid(index)) { return 0; }
-	if (length <= 0 || index < 0 || base < 0){ return 0; }
+	if (length <= 0 || index < 0 ){ return 0; }
 
 	visited[index] = 1;
 	permString[count] = array[index];
 
-	if (count == (length - 1)){
-		for (int i = 0; i < length; i++){
-			cout << permString[i] << " ";
-		}
-		std::cout<<endl;
-	}
+	if (count == (length - 1)){  for (int i = 0; i < length; i++){ cout << permString[i] << " "; } cout<<endl; }
 
 	int leftDir=0, rightDir = 0;
 	for (leftDir = index - 1; leftDir >= 0; leftDir--){
-		PermuteRec(array, length, leftDir, index,  count +1);
+		PermuteRec(array, length, leftDir, count +1);
 	}
 
 	for (rightDir= index + 1; rightDir <N; rightDir++){
-		PermuteRec(array, length, rightDir, index, count + 1);
+		PermuteRec(array, length, rightDir, count + 1);
 	}
 
 	visited[index] = 0;
