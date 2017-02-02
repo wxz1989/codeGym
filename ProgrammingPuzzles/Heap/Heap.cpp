81088bcc2ffc109b4e134a2176771b72ecc96ecf
Rakesh Gajjar <gajjarrakesh@users.noreply.github.com>
2017-02-02 19:02:18 +0530

    Issue fix and impl of Add API
    1. Heapify for heaps with 2 elements wasn't working
    2. Correct implementation of Add API. Now we can add one element at a time using Add method. For each element inserted Heapify would be invoked and Heap would maintain it's rule and Balance.

--- ProgrammingPuzzles/Heap/Heap.cpp
+++ ProgrammingPuzzles/Heap/Heap.cpp
@@ -84,7 +84,89 @@ Heap::Heap(size_t t){
 void Heap::Add(int value){
 	heapArray[currentIndex++] = value;
 	Heapify();
-	heapSize++;
+#include <iostream>
+#include <memory.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifndef READ_DATA_FROM_FILE
+//#define READ_DATA_FROM_FILE
+#endif
+
+using namespace std;
+
+#define INVALID -1
+#define MAX_HEAP_SIZE 1000
+
+void PrintHeapArray( int arrayp[], int size);
+void ResetHeapArray(int myArray[],int sizeOfHeap);
+
+class Heap{
+
+public:
+	Heap();
+	Heap(size_t size);
+
+private:
+	enum HeapType{
+		MIN_HEAP = 0,
+		MAX_HEAP,
+	};
+	int heapArray[MAX_HEAP_SIZE];
+
+	HeapType type;
+	size_t 	heapSize;
+public:
+	void Add(int value);
+	bool Remove(int value);
+	bool Update(int valueToUpdate, int newValue);
+
+	void BuildHeap(int a[], int N);
+		
+	int Min();
+	int Max();
+	int GetSize();
+
+	void Clear(void);
+
+	std::string ToString();
+
+	void SetHeapType(int type);
+	int GetHeapType(void);
+
+private:
+	void Heapify();
+	void HeaptifyUtil(int index);
+};
+
+
+void Heap::Clear(void){
+	memset(heapArray, 0, sizeof(int)* heapSize);
+	heapSize = 0;
+}
+
+void Heap::SetHeapType(int t){
+	type = (HeapType)t;
+}
+
+int Heap::GetHeapType(void){
+	return (HeapType)type;
+}
+
+Heap::Heap(){
+	type = (HeapType)0;
+	heapSize = 0;
+	memset(heapArray, 0, sizeof(int)* MAX_HEAP_SIZE);
+}
+Heap::Heap(size_t t){
+	type = (HeapType)t;
+	heapSize = 0;
+	memset(heapArray, 0, sizeof(int)* MAX_HEAP_SIZE);
+}
+
+void Heap::Add(int value){
+	heapArray[(heapSize++)+1] = value;
+	if ( heapSize > 1 ){  Heapify(); }	
 }
 bool Heap::Remove(int value){
 	return false;
@@ -93,9 +175,46 @@ bool Heap::Update(int valueToUpdate, int newValue){
 	return false;
 }
 
-void Heap::HeaptifyUtil(int parentIndex){
-	int pivot = INVALID;
+//ToDo:Implement Min using Delete Root from MinHeap and Max using Delete Root in MaxHeap
+int Heap::Min(){ return 0; }
+int Heap::Max(){ return 0; }
+int Heap::GetSize(){ return heapSize; }
 
+//Purpose of this function is to Copy value from the given HeapArray and use it for applying basic Heap algorithm HEAPIFY
+void Heap::BuildHeap(int a[], int N){
+	for (int i = 0; i< N; i++){ heapArray[i + 1] = a[i]; }
+	heapSize = N;
+	Heapify();
+}
+
+//Start from the middle of the heap and perform Heapify such that all element uptill root are covered
+void Heap::Heapify(){
+	bool bSwap = false;
+
+	if ( heapSize <= 1 ){ return; }
+
+	//ToDo: Improve on code qiality and algorithm here, shouldn't have to deal using two cases.
+	// Case for heap with more than 2 elements
+	if( heapSize > 2 ){
+		for (int i = heapSize/2; i>0; i--){ HeaptifyUtil(i); }	
+	} else {   	// Case for heap with more than <=2 elements
+		//as thre are only 2 elements compare the elements for Min/Max and Swap if required
+		if (type == MIN_HEAP){ 
+			if ( heapArray[1] > heapArray[2] ) {  bSwap = true; }
+		} else {
+			if ( heapArray[1] < heapArray[2] ) {  bSwap = true; }
+		}
+		if ( bSwap ){
+			int temp = heapArray[1];
+			heapArray[1] = heapArray[2];
+			heapArray[2] = temp;
+		}
+	}
+}
+
+void Heap::HeaptifyUtil(int parentIndex){
+
+	int pivot = INVALID;
 	int parent = heapArray[parentIndex];
 
 	int lCIndex = 2 * parentIndex;
@@ -132,23 +251,6 @@ void Heap::HeaptifyUtil(int parentIndex){
 	return;
 }
 
-//Purpose of this function is to Copy value from the given HeapArray and use it for applying basic Heap algorithm HEAPIFY
-void Heap::BuildHeap(int a[], int N){
-	for (int i = 0; i< N; i++){ heapArray[i + 1] = a[i]; }
-	heapSize = N;
-	Heapify();
-}
-
-//Start from the middle of the heap and perform Heapify such that all element uptill root are covered
-void Heap::Heapify(){
-	for (int i = heapSize/2; i>0; i--){  HeaptifyUtil(i); }
-}
-
-//ToDo:Implement Min using Delete Root from MinHeap and Max using Delete Root in MaxHeap
-int Heap::Min(){ return 0; }
-int Heap::Max(){ return 0; }
-int Heap::GetSize(){ return heapSize; }
-
 std::string Heap::ToString(){
 	std::string retValue;
 	/*int index = 0;
@@ -167,6 +269,72 @@ std::string Heap::ToString(){
 	int i;
 	for (i = 1; i < heapSize; i++){
 		std::cout << heapArray[i] << ",";
+	}
+	std::cout << heapArray[i] << "]" <<  std::endl;
+	return retValue;
+}
+
+
+int main(){
+
+	Heap mHeap(0);
+
+	int myArray[MAX_HEAP_SIZE] = { 0 };
+	int test_cases = 0;
+	int sizeOfHeap = 0;
+
+#ifdef READ_DATA_FROM_FILE
+	freopen("HeapSampleInput.txt", "r", stdin);
+	cin >> test_cases;
+
+	for (int tc = 0; tc < test_cases; tc++){
+		
+		cin >> sizeOfHeap;
+
+		for (int i = 0; i < sizeOfHeap; i++){
+			cin >> myArray[i];
+			mHeap.Add(myArray[i]);
+		}
+
+		cout <<"========================"<< endl;
+		PrintHeapArray(myArray, sizeOfHeap);
+		//mHeap.BuildHeap(myArray, sizeOfHeap);
+		mHeap.ToString();
+		mHeap.Clear();
+		ResetHeapArray(myArray, sizeOfHeap);
+		//mHeap.GetHeapType() == 0 ? mHeap.SetHeapType(1) : mHeap.SetHeapType(0);
+	}
+#else
+	srand(time(NULL));
+	sizeOfHeap = rand()%10;
+	srand(time(NULL));
+
+	for ( int i = 0; i< sizeOfHeap;i++){
+		int heapValue = rand()%20;
+		myArray[i] = heapValue;	
+		mHeap.Add(myArray[i]);
+	}
+
+	cout <<"========================"<< endl;
+	PrintHeapArray(myArray, sizeOfHeap);
+	//mHeap.BuildHeap(myArray, sizeOfHeap);
+	mHeap.ToString();
+	mHeap.Clear();
+	ResetHeapArray(myArray, sizeOfHeap);
+#endif
+	return 0;
+}
+
+void ResetHeapArray(int array[], int size){
+	for (int i = 0; i<size;i++){array[i]  = 0; }
+}
+void PrintHeapArray( int array[], int size){
+	int i;
+
+	cout << "Org Heap Array:[";
+	for ( i = 0; i<size-1;i++){ cout << array[i] << ","; }
+	cout << array[i] << "]" << endl;
+}
 	}
 	std::cout << heapArray[i] << "]" <<  std::endl;
 	return retValue;
