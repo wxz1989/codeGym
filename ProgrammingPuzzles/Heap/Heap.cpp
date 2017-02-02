2a50b622dd0d33c4797cd23968c1ba8f00de149f
lifelongstudent <rakeshgajjar1983@gmail.com>
2017-02-02 23:10:52 +0530

    1. GetMin, GetMax Implemented 2. Tested for all possible inputs

--- ProgrammingPuzzles/Heap/Heap.cpp
+++ ProgrammingPuzzles/Heap/Heap.cpp
@@ -4,92 +4,6 @@
 #include <stdio.h>
 
 #ifndef READ_DATA_FROM_FILE
-#define READ_DATA_FROM_FILE
-#endif
-
-using namespace std;
-
-#define INVALID -1
-#define MAX_HEAP_SIZE 1000
-
-void PrintHeapArray( int arrayp[], int size);
-
-class Heap{
-
-public:
-	Heap();
-	Heap(size_t size);
-
-private:
-	enum HeapType{
-		MIN_HEAP = 0,
-		MAX_HEAP,
-	};
-	int heapArray[MAX_HEAP_SIZE];
-
-	HeapType type;
-	size_t 	heapSize;
-	int 	currentIndex;
-public:
-	void Add(int value);
-	bool Remove(int value);
-	bool Update(int valueToUpdate, int newValue);
-
-	void BuildHeap(int a[], int N);
-		
-	int Min();
-	int Max();
-	int GetSize();
-
-	void Clear(void);
-
-	std::string ToString();
-
-	void SetHeapType(int type);
-	int GetHeapType(void);
-
-private:
-	void Heapify();
-	void HeaptifyUtil(int index);
-};
-
-
-void Heap::Clear(void){
-	memset(heapArray, 0, sizeof(int)* heapSize);
-	currentIndex = 0;
-	heapSize = 0;
-}
-
-void Heap::SetHeapType(int t){
-	type = (HeapType)t;
-}
-
-int Heap::GetHeapType(void){
-	return (HeapType)type;
-}
-
-Heap::Heap(){
-	currentIndex = 0;
-	type = (HeapType)0;
-	heapSize = 0;
-	memset(heapArray, 0, sizeof(int)* MAX_HEAP_SIZE);
-}
-Heap::Heap(size_t t){
-	type = (HeapType)t;
-	currentIndex = 0;
-	heapSize = 0;
-	memset(heapArray, 0, sizeof(int)* MAX_HEAP_SIZE);
-}
-
-void Heap::Add(int value){
-	heapArray[currentIndex++] = value;
-	Heapify();
-#include <iostream>
-#include <memory.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-#ifndef READ_DATA_FROM_FILE
 //#define READ_DATA_FROM_FILE
 #endif
 
@@ -123,11 +37,14 @@ public:
 
 	void BuildHeap(int a[], int N);
 		
-	int Min();
-	int Max();
+	int GetMin();
+	int GetMax();
 	int GetSize();
 
 	void Clear(void);
+
+	void SetHeapSize(size_t size);
+	size_t GetHeapSize(void);
 
 	std::string ToString();
 
@@ -140,9 +57,18 @@ private:
 };
 
 
+void Heap::SetHeapSize(size_t size){
+	heapSize = size;
+}
+
+size_t Heap::GetHeapSize(void){
+	return heapSize;
+}
+
 void Heap::Clear(void){
 	memset(heapArray, 0, sizeof(int)* heapSize);
 	heapSize = 0;
+	SetHeapSize(0);
 }
 
 void Heap::SetHeapType(int t){
@@ -176,8 +102,31 @@ bool Heap::Update(int valueToUpdate, int newValue){
 }
 
 //ToDo:Implement Min using Delete Root from MinHeap and Max using Delete Root in MaxHeap
-int Heap::Min(){ return 0; }
-int Heap::Max(){ return 0; }
+int Heap::GetMin(){ 
+
+	if ( heapSize == 0 ) { cout << "Error: Empty Heap" << endl; return -1;}
+	if ( type != MIN_HEAP){ cout << "Invalid request, this is MIN HEAP" << endl; return -1; }
+
+	int minValue = heapArray[1];
+	for( int i = 1; i<heapSize+1; i++){ heapArray[i] = heapArray[i+1]; }
+	heapSize--;
+
+	Heapify();
+
+	return minValue; 
+}
+int Heap::GetMax(){ 
+	if ( heapSize == 0 ) { cout << "Error :Empty Heap" << endl; return -1;}
+	if ( type != MAX_HEAP){ cout << "Error: Invalid request, this is MAX HEAP" << endl; return -1; }
+
+	int minValue = heapArray[1];
+	for( int i = 1; i<heapSize; i++){ heapArray[i] = heapArray[i+1]; }
+	heapSize--;
+
+	Heapify();
+
+	return minValue; 
+}
 int Heap::GetSize(){ return heapSize; }
 
 //Purpose of this function is to Copy value from the given HeapArray and use it for applying basic Heap algorithm HEAPIFY
@@ -252,17 +201,7 @@ void Heap::HeaptifyUtil(int parentIndex){
 }
 
 std::string Heap::ToString(){
-	std::string retValue;
-	/*int index = 0;
-
-	retValue.append("[");
-	char buffer[100];
-	for (index = 1; index < heapSize; index++){
-	sprintf(buffer, "%d,", heapArray[index]);
-	retValue.append(buffer);
-	}
-	sprintf(buffer, "%d]", heapArray[index]);
-	retValue.append(buffer);*/
+	std::string retValue("");
 	
 	std::cout << "Size:" << heapSize << std::endl;
 	std::cout << "Heap:[";
@@ -291,23 +230,27 @@ int main(){
 		
 		cin >> sizeOfHeap;
 
-		for (int i = 0; i < sizeOfHeap; i++){
+		mHeap.SetHeapSize(sizeOfHeap);
+
+		for (int i = 0; i < mHeap.GetHeapSize(); i++){
 			cin >> myArray[i];
 			mHeap.Add(myArray[i]);
 		}
 
 		cout <<"========================"<< endl;
-		PrintHeapArray(myArray, sizeOfHeap);
+		PrintHeapArray(myArray, mHeap.GetHeapSize());
 		//mHeap.BuildHeap(myArray, sizeOfHeap);
 		mHeap.ToString();
 		mHeap.Clear();
-		ResetHeapArray(myArray, sizeOfHeap);
+		ResetHeapArray(myArray, mHeap.GetHeapSize());
 		//mHeap.GetHeapType() == 0 ? mHeap.SetHeapType(1) : mHeap.SetHeapType(0);
 	}
 #else
 	srand(time(NULL));
 	sizeOfHeap = rand()%10;
 	srand(time(NULL));
+
+	//mHeap.SetHeapSize(sizeOfHeap);
 
 	for ( int i = 0; i< sizeOfHeap;i++){
 		int heapValue = rand()%20;
@@ -316,76 +259,30 @@ int main(){
 	}
 
 	cout <<"========================"<< endl;
-	PrintHeapArray(myArray, sizeOfHeap);
+	PrintHeapArray(myArray, mHeap.GetHeapSize());
 	//mHeap.BuildHeap(myArray, sizeOfHeap);
 	mHeap.ToString();
-	mHeap.Clear();
-	ResetHeapArray(myArray, sizeOfHeap);
+	//mHeap.Clear();
+	//ResetHeapArray(myArray, sizeOfHeap);
+
+	while ( mHeap.GetSize() > 0 ){
+		int min = -1;
+		min = mHeap.GetMin();
+		if( min != -1 ){
+			cout << "Min: " << min << endl;
+		} else {
+			cout << "Invalid min" << endl;
+		}
+		//mHeap.ToString();
+	}
+
+
 #endif
 	return 0;
 }
 
 void ResetHeapArray(int array[], int size){
-	for (int i = 0; i<size;i++){array[i]  = 0; }
-}
-void PrintHeapArray( int array[], int size){
-	int i;
-
-	cout << "Org Heap Array:[";
-	for ( i = 0; i<size-1;i++){ cout << array[i] << ","; }
-	cout << array[i] << "]" << endl;
-}
-	}
-	std::cout << heapArray[i] << "]" <<  std::endl;
-	return retValue;
-}
-
-
-int main(){
-
-	Heap mHeap(0);
-
-	int myArray[MAX_HEAP_SIZE] = { 0 };
-	int test_cases = 0;
-	int sizeOfHeap = 0;
-
-#ifdef READ_DATA_FROM_FILE
-	freopen("HeapSampleInput.txt", "r", stdin);
-	cin >> test_cases;
-
-	for (int tc = 0; tc < test_cases; tc++){
-		
-		cin >> sizeOfHeap;
-
-		for (int i = 0; i < sizeOfHeap; i++){
-			cin >> myArray[i];
-		}
-
-		cout <<"========================"<< endl;
-		PrintHeapArray(myArray, sizeOfHeap);
-		mHeap.BuildHeap(myArray, sizeOfHeap);
-		mHeap.ToString();
-		mHeap.Clear();
-		//mHeap.GetHeapType() == 0 ? mHeap.SetHeapType(1) : mHeap.SetHeapType(0);
-	}
-#else
-	srand(time(NULL));
-	sizeOfHeap = rand()%20;
-	srand(time(NULL));
-
-	for ( int i = 0; i< sizeOfHeap;i++){
-		int heapValue = rand()%99;
-		myArray[i] = heapValue;	
-	}
-
-	cout <<"========================"<< endl;
-	PrintHeapArray(myArray, sizeOfHeap);
-	mHeap.BuildHeap(myArray, sizeOfHeap);
-	mHeap.ToString();
-	mHeap.Clear();
-
-#endif
-	return 0;
+	for ( int i = 0; i<size;i++){array[i]  = 0; }
 }
 
 void PrintHeapArray( int array[], int size){
