a5ca3e5f651e9ddfccd28b88af510a8e5c8bd040
Rakesh Gajjar <gajjarrakesh@users.noreply.github.com>
2017-02-06 18:26:21 +0530

    Stabalization
    1.Dynamicaly growing heap, double it's space when reached to it;s capacity 2. Added HeapSort function 3. Changes done for making heap dynamically growing

--- ProgrammingPuzzles/Heap/HeapDriver.cpp
+++ ProgrammingPuzzles/Heap/HeapDriver.cpp
@@ -7,21 +7,21 @@
 
 using namespace std;
 
-void PrintHeapArray( int arrayp[], int size);
-void ResetHeapArray(int myArray[],int sizeOfHeap);
+void PrintHeapArray(int arrayp[], int size);
+void ResetHeapArray(int myArray[], int sizeOfHeap);
 
 void ResetHeapArray(int array[], int size){
-	for ( int i = 0; i<size;i++){array[i]  = 0; }
+	for (int i = 0; i<size; i++){ array[i] = 0; }
 }
 
-void PrintHeapArray( int array[], int size){
+void PrintHeapArray(int array[], int size){
 	int i;
-	cout << "Org Heap Array[" << size << "]:[" ;
-	for ( i = 0; i<size-1;i++){ cout << array[i] << ","; }
+	cout << "Org Heap Array[" << size << "]:[";
+	for (i = 0; i<size - 1; i++){ cout << array[i] << ","; }
 	cout << array[i] << "]" << endl;
 }
 
-void Test_SampleInput(Heap& heapArg );
+void Test_SampleInput(Heap& heapArg);
 void Test_RandomisedDataInput(Heap& heapArg);
 
 void Test_SampleInput(Heap& heapArg){
@@ -34,35 +34,39 @@ void Test_SampleInput(Heap& heapArg){
 	cin >> test_cases;
 
 	for (int tc = 0; tc < test_cases; tc++){
-		
+
 		cin >> sizeOfHeap;
 
 		//mHeap.SetHeapSize(sizeOfHeap);
 		for (int i = 0; i < sizeOfHeap; i++){
 			cin >> myArray[i];
-			heapArg.Add(myArray[i]);
+			//heapArg.Add(myArray[i]);
 		}
 		//mHeap.SetHeapSize(sizeOfHeap);
-		cout <<"# "<< tc+1 << endl;
-		PrintHeapArray(myArray, heapArg.GetHeapSize());
-		//mHeap.BuildHeap(myArray, sizeOfHeap);
-		//mHeap.PrintHeap();
-		cout << heapArg.ToString() << endl;
-		
+		cout << "# " << tc + 1 << endl;
+		heapArg.BuildHeap(myArray, sizeOfHeap);
+
+
+		PrintHeapArray(myArray, heapArg.GetHeapSize());		//Because Add is not calld priot to Build Heap 
+		//heapArg.PrintHeap();
+		std::string stringHeap= heapArg.ToString();
+		cout <<stringHeap << endl;
+
 		//mHeap.GetHeapType() == 0 ? mHeap.SetHeapType(1) : mHeap.SetHeapType(0);
 		/*while ( mHeap.GetSize() > 0 ){
-			int min = INVALID;
-			min = mHeap.Delete();
-			if( min != INVALID ){
-				cout << "Min: " << min << endl;
-			} else {
-				cout << "Invalid min" << endl;
-			}
-			//mHeap.PrintHeap();
-			cout << mHeap.ToString() << endl;
+		int min = INVALID;
+		min = mHeap.Delete();
+		if( min != INVALID ){
+		cout << "Min: " << min << endl;
+		} else {
+		cout << "Invalid min" << endl;
+		}
+		//mHeap.PrintHeap();
+		cout << mHeap.ToString() << endl;
 		}*/
 
 		heapArg.HeapSort();
+		//heapArg.TestHS();
 
 		heapArg.Clear();
 		ResetHeapArray(myArray, heapArg.GetHeapSize());
@@ -75,41 +79,30 @@ void Test_RandomisedDataInput(Heap& heapArg){
 	int sizeOfHeap = 0;
 
 	srand(time(NULL));
-	sizeOfHeap = rand()%10;
+	sizeOfHeap = rand() % 10;
 	srand(time(NULL));
 
 	//mHeap.SetHeapSize(sizeOfHeap);
 
-	for ( int i = 0; i< sizeOfHeap;i++){
-		int heapValue = rand()%30;
-		myArray[i] = heapValue;	
+	for (int i = 0; i< sizeOfHeap; i++){
+		int heapValue = rand() % 30;
+		myArray[i] = heapValue;
 		heapArg.Add(myArray[i]);
 	}
 
-	cout <<"========================"<< endl;
+	cout << "========================" << endl;
 	PrintHeapArray(myArray, heapArg.GetHeapSize());
 	//heapArg.BuildHeap(myArray, sizeOfHeap);
-	heapArg.PrintHeap();
+	//heapArg.PrintHeap();
 	//heapArg.Clear();
 	//ResetHeapArray(myArray, sizeOfHeap);
 
+	
+	std::string stringHeap= heapArg.ToString();
+	cout <<stringHeap << endl;
+
 	heapArg.HeapSort();
 	heapArg.Clear();
-
-	/*
-	while ( mHeap.GetSize() > 0 ){
-		int min = INVALID;
-		min = mHeap.Delete();
-		if( min != INVALID ){
-			cout << "Min: " << min << endl;
-		} else {
-			cout << "Invalid min" << endl;
-		}
-		//mHeap.PrintHeap();
-		cout << mHeap.ToString() << endl;
-	}
-	*/
-
 }
 
 int main(int argc, char* argv[]){
@@ -118,13 +111,15 @@ int main(int argc, char* argv[]){
 	mHeap.Init();
 
 	cout << "Arg Count:" << argc << endl;
-	if ( argc > 1){
-		cout << "Arg :" << argv[1]<< endl;
-		if ( strcmp(argv[1],  "0") == 0 ){
-			Test_SampleInput(mHeap);		
-		}else if ( strcmp(argv[1],  "1") == 0){
+	if (argc > 1){
+		cout << "Arg :" << argv[1] << endl;
+		if (strcmp(argv[1], "0") == 0){
+			Test_SampleInput(mHeap);
+		}
+		else if (strcmp(argv[1], "1") == 0){
 			Test_RandomisedDataInput(mHeap);
-		} else {
+		}
+		else {
 			Test_SampleInput(mHeap);
 		}
 	}
