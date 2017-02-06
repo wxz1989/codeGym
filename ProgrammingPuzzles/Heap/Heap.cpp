c9f085880e954d819f512936c8d1a05d44cb41ee
Rakesh Gajjar <gajjarrakesh@users.noreply.github.com>
2017-02-06 18:24:47 +0530

    Stabalization
    1. heap Resize wsa invoked when heapSize was reaching beyond current heapCapacity
    2. Tested and corrected resize with Build and Add APIs

--- ProgrammingPuzzles/Heap/Heap.cpp
+++ ProgrammingPuzzles/Heap/Heap.cpp
@@ -20,10 +20,13 @@ Heap::Heap(HeapType hType, size_t cap){
 
 Heap::~Heap(){
 
-	delete [] pHeapArray;
+	delete[] pHeapArray;
 }
 
 void Heap::Init(void){
+
+	if ( pHeapArray != NULL ){  delete [] pHeapArray; }
+
 	pHeapArray = new int[heapCapacity];
 	memset(pHeapArray, 0, sizeof(int)* heapCapacity);
 }
@@ -34,12 +37,12 @@ void Heap::Clear(void){
 
 void Heap::Add(int value){
 
-	pHeapArray[heapSize+1] = value;
+	pHeapArray[heapSize + 1] = value;
 	heapSize++;
-	if ( heapSize == heapCapacity){
+	if (heapSize == heapCapacity-1){
 		Resize();
 	}
-	if ( heapSize > 1 ){  Heapify(); }	
+	if (heapSize > 1){ Heapify(); }
 }
 bool Heap::Remove(int value){
 	return false;
@@ -68,7 +71,7 @@ bool Heap::IsValid(int childIndex /*, int parent*/){
 
 	bool retValue = false;
 
-	if( childIndex <= heapSize  /*&& parent <= heapSize*/ ){ retValue=true; }
+	if (childIndex <= heapSize  /*&& parent <= heapSize*/){ retValue = true; }
 	else { retValue = false; }
 
 	return retValue;
@@ -80,47 +83,54 @@ int Heap::GetSwapIndex(int lIndex, int rIndex, int parent){
 	int pivotIndex = INVALID;
 
 	//Missed one condition : If Only one of the Child exists and not two?
-	if ( mHeapType == Heap::MIN_HEAP ){
-		if ( IsValid(lIndex) && IsValid(rIndex)){
+	if (mHeapType == Heap::MIN_HEAP){
+		if (IsValid(lIndex) && IsValid(rIndex)){
 			int left = pHeapArray[lIndex];
 			int right = pHeapArray[rIndex];
 
-			if ( left < right){
+			if (left < right){
 				index = lIndex;
-			} else {
+			}
+			else {
 				index = rIndex;
 			}
-		} else if ( IsValid(lIndex)){
+		}
+		else if (IsValid(lIndex)){
 			index = lIndex;
-		} else if ( IsValid(rIndex)){
+		}
+		else if (IsValid(rIndex)){
 			index = rIndex;
 		}
 
-		if ( index != INVALID ){ if( pHeapArray[parent] > pHeapArray[index] ){ pivotIndex=index; } }
-	} else {
-		if ( IsValid(lIndex) && IsValid(rIndex)){
+		if (index != INVALID){ if (pHeapArray[parent] > pHeapArray[index]){ pivotIndex = index; } }
+	}
+	else {
+		if (IsValid(lIndex) && IsValid(rIndex)){
 			int left = pHeapArray[lIndex];
 			int right = pHeapArray[rIndex];
-			
-			if ( left > right){
+
+			if (left > right){
 				index = lIndex;
-			} else {
+			}
+			else {
 				index = rIndex;
 			}
-		} else if ( IsValid(lIndex)){
+		}
+		else if (IsValid(lIndex)){
 			index = lIndex;
-		} else if( IsValid(rIndex)){
+		}
+		else if (IsValid(rIndex)){
 			index = rIndex;
 		}
-		if ( index != INVALID ){ if( pHeapArray[parent] < pHeapArray[index] ){ pivotIndex=index; }}
+		if (index != INVALID){ if (pHeapArray[parent] < pHeapArray[index]){ pivotIndex = index; } }
 	}
 	return pivotIndex;
 }
 
 //ToDo:Implement Min using Delete Root from MinHeap and Max using Delete Root in MaxHeap
-int Heap::Delete(){ 
+int Heap::Delete(){
 
-	if ( heapSize == 0 ) { cout << "Error: Empty Heap" << endl; return INVALID;}
+	if (heapSize == 0) { cout << "Error: Empty Heap" << endl; return INVALID; }
 
 	int minValue = pHeapArray[1];
 	//for( int i = 1; i<heapSize+1; i++){ pHeapArray[i] = pHeapArray[i+1]; }
@@ -132,14 +142,14 @@ int Heap::Delete(){
 
 	Heapify();
 
-	return minValue; 
+	return minValue;
 }
 
 int Heap::GetSize(){ return heapSize; }
 
 //Purpose of this function is to Copy value from the given HeapArray and use it for applying basic Heap algorithm HEAPIFY
 void Heap::BuildHeap(int a[], int N){
-	if ( N > heapCapacity){ Resize(); }
+	if ( N >= heapCapacity){ Resize(); }
 	for (int i = 0; i< N; i++){ pHeapArray[i + 1] = a[i]; }
 	heapSize = N;
 	Heapify();
@@ -148,20 +158,22 @@ void Heap::BuildHeap(int a[], int N){
 //Start from the middle of the heap and perform Heapify such that all element uptill root are covered
 void Heap::Heapify(){
 	bool bSwap = false;
-	if ( heapSize <= 1 ){ return; }
+	if (heapSize <= 1){ return; }
 
 	//ToDo: Improve on code qiality and algorithm here, shouldn't have to deal using two cases.
 	// Case for heap with more than 2 elements
-	if( heapSize > 2 ){
-		for (int i = heapSize/2; i>0; i--){ HeaptifyUtil(i); }	
-	} else {   	// Case for heap with more than <=2 elements
+	if (heapSize > 2){
+		for (int i = heapSize / 2; i>0; i--){ HeaptifyUtil(i); }
+	}
+	else {   	// Case for heap with more than <=2 elements
 		//as thre are only 2 elements compare the elements for Min/Max and Swap if required
-		if (mHeapType == MIN_HEAP){ 
-			if ( pHeapArray[1] > pHeapArray[2] ) {  bSwap = true; }
-		} else {
-			if ( pHeapArray[1] < pHeapArray[2] ) {  bSwap = true; }
+		if (mHeapType == MIN_HEAP){
+			if (pHeapArray[1] > pHeapArray[2]) { bSwap = true; }
 		}
-		if ( bSwap ){
+		else {
+			if (pHeapArray[1] < pHeapArray[2]) { bSwap = true; }
+		}
+		if (bSwap){
 			int temp = pHeapArray[1];
 			pHeapArray[1] = pHeapArray[2];
 			pHeapArray[2] = temp;
@@ -171,7 +183,7 @@ void Heap::Heapify(){
 
 void Heap::HeaptifyUtil(int parentIndex){
 
-	if ( parentIndex <=0 || parentIndex > heapSize  ){ return; }
+	if (parentIndex <= 0 || parentIndex > heapSize){ return; }
 
 	int pivot = INVALID;
 
@@ -189,7 +201,7 @@ void Heap::HeaptifyUtil(int parentIndex){
 	// If it does than select the curent Parent as Pivot and swap Parent and Child values.
 
 	pivot = GetSwapIndex(lCIndex, rCIndex, parentIndex);
-	if ( pivot >0 && pivot <=heapSize ){
+	if (pivot >0 && pivot <= heapSize){
 		int temp = pHeapArray[parentIndex];
 		pHeapArray[parentIndex] = pHeapArray[pivot];
 		pHeapArray[pivot] = temp;
@@ -200,74 +212,74 @@ void Heap::HeaptifyUtil(int parentIndex){
 
 std::string Heap::ToString(void){
 	std::string retValue("");
-	
-	char heapString[] = {0};
 
-	sprintf(heapString, "%s", "Heap:[");
-	retValue.append(heapString);
+	//char heapString[] = { 0 };
+	char* pHeapString = NULL;
+	pHeapString = new char[2];
+
+	sprintf(pHeapString, "%s", "Heap:[");
+	retValue.append(pHeapString);
 
 	int i;
-	for (i = 1; i < heapSize; i++){
-		sprintf(heapString, "%d%c", pHeapArray[i], ',');
-		retValue.append(heapString);
+	for (i = 1; i <heapSize; i++){
+		sprintf(pHeapString, "%d%c", pHeapArray[i], ',');
+		retValue.append(pHeapString);
 	}
-	sprintf(heapString, "%d%c", pHeapArray[i], ']');
-	retValue.append(heapString);
+	sprintf(pHeapString, "%d%c", pHeapArray[i], ']');
+	retValue.append(pHeapString);
+
+	delete []pHeapString;
 
 	return retValue;
 }
 
 void Heap::PrintHeap(void){
-	
+
 	std::cout << "Size:" << heapSize << std::endl;
 	std::cout << "Heap:[";
 	int i;
 	for (i = 1; i < heapSize; i++){
 		std::cout << pHeapArray[i] << ",";
 	}
-	std::cout << pHeapArray[i] << "]" <<  std::endl;
+	std::cout << pHeapArray[i] << "]" << std::endl;
 }
 
 void Heap::HeapSort(void){
 
-	cout << "HeapSort>>Enter" << endl;
-	int start=1, end =heapSize;
+	int start = 1, end = heapSize;
 
 	int index = 0;
-	int* sorted= new int[heapSize];
+	int* sorted = new int[heapSize];
+	memset(sorted, 0, sizeof(int)* heapSize);
 
-	while ( GetSize() > 0 ){
+	while (GetSize() > 0){
 		int min = INVALID;
 		min = Delete();
-		if( min != INVALID ){
+		if (min != INVALID){
 			sorted[index++] = min;
 		}
 	}
 
 	cout << "HeapSort:[";
 	int i;
-	for (i = 0; i < index-1;i++){
+	for (i = 0; i < index - 1; i++){
 		cout << sorted[i] << ", ";
 	}
 	cout << sorted[i] << "] " << endl;
 
-	delete [] sorted;
+	delete[] sorted;
 	sorted = NULL;
-	cout << "HeapSort<<Exit" << endl;
 }
 
 void Heap::Resize(void){
 
-	cout << "Resize Called"<< endl;
-
-	heapCapacity *=2; 
+	heapCapacity *= 2;
 	int* pBackUp = new int[heapCapacity];
-	memcpy(pBackUp, pHeapArray, sizeof(int)* heapSize);
+	memset(pBackUp, 0, sizeof(int)* (heapSize + 1));
+	memcpy(pBackUp, pHeapArray, sizeof(int)*(heapSize+1));
 
-	delete [] pHeapArray;
+	delete[] pHeapArray;
 	pHeapArray = pBackUp;
-	
-	cout << "Resized Heap Size :"<< heapCapacity << endl;
 }
 
 void Heap::SetHeapCapacity(size_t cap){
@@ -277,3 +289,39 @@ size_t Heap::GetHeapCapacity(void){
 	return heapCapacity;
 }
 
+//By Calling this function you are taking top most element from heap and hence reducing size of heap
+int Heap::GetTop(){
+
+	std::string stringHeap("");
+	while ( GetSize() > 0 ){
+		int min = INVALID;
+		min = Delete();
+		if( min != INVALID ){
+			cout << "Min: " << min << endl;
+		} else {
+			cout << "Invalid min" << endl;
+		}
+		stringHeap = ToString();
+		cout << stringHeap << endl;
+	}
+}
+
+/*
+void Heap::TestHS()
+{
+	int length = heapSize;
+	int temp = INVALID;
+	Resize();
+  for(int i=heapSize; i >= 0; i--)
+  {
+    temp = pHeapArray[0];
+    pHeapArray[0] = pHeapArray[heapSize];
+    pHeapArray[heapSize] = temp;
+    heapSize--;
+
+    Heapify();
+    
+  }
+  for(int i=0; i < length; i++) { cout << pHeapArray[i] << " " ; }
+}
+*/
