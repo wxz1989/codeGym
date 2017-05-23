34d9451d897b233618d7ac2ffd1bdecfdf185cd7
Rakesh Gajjar <gajjarrakesh@users.noreply.github.com>
2017-05-23 16:48:34 +0530

    Google Authentication form node-express Server

---
+++ nodeSources/GoogleOAuthServer.js
@@ -0,0 +1,83 @@
+var http = require('http');
+var express = require('express');
+var Session = require('express-session');
+var google = require('googleapis');
+var plus = google.plus('v1');
+var OAuth2 = google.auth.OAuth2;
+const ClientId = "947156970260-36nhr0l2nbvlbl723m5ehjolubi9vnub.apps.googleusercontent.com";
+const ClientSecret = "S0c4a5YlTENZvHtLrUqO7iCo";
+const RedirectionUrl = "http://localhost:8080/oauthCallback";
+
+var app = express();
+app.use(Session({
+    secret: 'rakesh-secret-19830622226',
+    resave: true,
+    saveUninitialized: true
+}));
+
+function getOAuthClient () {
+    return new OAuth2(ClientId ,  ClientSecret, RedirectionUrl);
+}
+
+function getAuthUrl () {
+    var oauth2Client = getOAuthClient();
+    // generate a url that asks permissions for Google+ and Google Calendar scopes
+    var scopes = [
+      'https://www.googleapis.com/auth/plus.me'
+    ];
+
+    var url = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: scopes // If you only need one scope you can pass it as string
+    });
+
+    return url;
+}
+
+app.use("/oauthCallback", function (req, res) {
+    var oauth2Client = getOAuthClient();
+    var session = req.session;
+    var code = req.query.code;
+    oauth2Client.getToken(code, function(err, tokens) {
+      // Now tokens contains an access_token and an optional refresh_token. Save them.
+      if(!err) {
+        oauth2Client.setCredentials(tokens);
+        session["tokens"]=tokens;
+        res.send("Login successful!!" + "Token" + JSON.stringify(tokens) + "Sessios:" + JSON.stringify(session));
+        //&lt;a href="/details"&gt;Go to details page&lt;/a&gt;      
+        }
+      else{
+        res.send("Login failed!");
+      }
+    });
+});
+
+app.use("/details", function (req, res) {
+    var oauth2Client = getOAuthClient();
+    oauth2Client.setCredentials(req.session["tokens"]);
+
+    var p = new Promise(function (resolve, reject) {
+        plus.people.get({ userId: 'me', auth: oauth2Client }, function(err, response) {
+            resolve(response || err);
+        });
+    }).then(function (data) {
+        /*res.send(`
+            &lt;img src=${data.image.url} /&gt;
+            &lt;h3&gt;Hello ${data.displayName}&lt;/h3&gt;
+        `);*/
+    })
+});
+
+app.use("/", function (req, res) {
+    var url = getAuthUrl();
+    res.send("Authentication using google oAuth!" + "URL:[" + url + "]");
+    //window.open(url,'mywindow','width=400,height=200')
+});
+
+
+var port = 8080;
+var server = http.createServer(app);
+server.listen(port);
+server.on('listening', function () {
+    console.log("listening to "+ port);
+});
