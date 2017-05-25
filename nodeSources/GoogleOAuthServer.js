9df5174219cc689311ddbff37a6f561085a25047
Rakesh Gajjar <gajjarrakesh@users.noreply.github.com>
2017-05-25 15:30:56 +0530

    Updated to show indian holidays
    Updated code with correct auth and calendarId

--- nodeSources/GoogleOAuthServer.js
+++ nodeSources/GoogleOAuthServer.js
@@ -10,111 +10,131 @@ const RedirectionUrl = "http://localhost:8080/oauthCallback";
 
 var app = express();
 app.use(Session({
-    secret: 'rakesh-secret-19830622226',
-    resave: true,
-    saveUninitialized: true
+	secret: 'rakesh-secret-19830622226',
+	resave: true,
+	saveUninitialized: true
 }));
 
 function getOAuthClient () {
-    return new OAuth2(ClientId ,  ClientSecret, RedirectionUrl);
+	return new OAuth2(ClientId ,  ClientSecret, RedirectionUrl);
 }
 
 function getAuthUrl () {
-    var oauth2Client = getOAuthClient();
-    // generate a url that asks permissions for Google+ and Google Calendar scopes
-    var scopes = [
-      'https://www.googleapis.com/auth/plus.me',
-      'https://www.googleapis.com/auth/calendar',
-      'https://www.googleapis.com/auth/calendar.readonly'
-    ];
+	var oauth2Client = getOAuthClient();
+	// generate a url that asks permissions for Google+ and Google Calendar scopes
+	var scopes = [
+	'https://www.googleapis.com/auth/plus.me',
+	'https://www.googleapis.com/auth/calendar',
+	'https://www.googleapis.com/auth/calendar.readonly'
+	];
 
-    var url = oauth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: scopes // If you only need one scope you can pass it as string
-    });
+	var url = oauth2Client.generateAuthUrl({
+		access_type: 'offline',
+		scope: scopes // If you only need one scope you can pass it as string
+	});
 
-    return url;
+	return url;
 }
 
 app.use("/oauthCallback", function (req, res) {
-    var oauth2Client = getOAuthClient();
-    var session = req.session;
-    var code = req.query.code;
-    oauth2Client.getToken(code, function(err, tokens) {
-      // Now tokens contains an access_token and an optional refresh_token. Save them.
-      if(!err) {
-        oauth2Client.setCredentials(tokens);
-        session["tokens"]=tokens;
-        res.send("Login successful!!" + "Token:[" + JSON.stringify(tokens) + "]"+ " Sessions:[" + JSON.stringify(session) + "]");
-        //&lt;a href="/details"&gt;Go to details page&lt;/a&gt;      
-        }
-      else{
-        res.send("Login failed!");
-      }
-    });
+	var oauth2Client = getOAuthClient();
+	var session = req.session;
+	var code = req.query.code;
+	oauth2Client.getToken(code, function(err, tokens) {
+	  // Now tokens contains an access_token and an optional refresh_token. Save them.
+	  if(!err) {
+	  	oauth2Client.setCredentials(tokens);
+	  	session["tokens"]=tokens;
+	  	res.send("Login successful!!" + "Token:[" + JSON.stringify(tokens) + "]"+ " Sessions:[" + JSON.stringify(session) + "]");
+		//&lt;a href="/details"&gt;Go to details page&lt;/a&gt;      
 
-    listEvents(session["tokens"]);
+		console.log("Tokens Sending to list Function:" + JSON.stringify(session["tokens"]));
+		listEvents(oauth2Client);
+	}
+	else{
+		res.send("Login failed!");
+		return;
+	}
 
+});
+
+	//console.log("*************Saved Token:" + "Session:" + JSON.stringify(session) +  "Strigified Tokens:[" + JSON.stringify(session["tokens"]) + "] **********************");
+	//listEvents(session["tokens"]);
 });
 
 app.use("/details", function (req, res) {
-    var oauth2Client = getOAuthClient();
-    oauth2Client.setCredentials(req.session["tokens"]);
+	var oauth2Client = getOAuthClient();
+	oauth2Client.setCredentials(req.session["tokens"]);
 
-    var p = new Promise(function (resolve, reject) {
-        plus.people.get({ userId: 'me', auth: oauth2Client }, function(err, response) {
-            resolve(response || err);
-        });
-    }).then(function (data) {
-        /*res.send(`
-            &lt;img src=${data.image.url} /&gt;
-            &lt;h3&gt;Hello ${data.displayName}&lt;/h3&gt;
-        `);*/
-    })
+	var p = new Promise(function (resolve, reject) {
+		plus.people.get({ userId: 'me', auth: oauth2Client }, function(err, response) {
+			resolve(response || err);
+		});
+	}).then(function (data) {
+		/*res.send(`
+			&lt;img src=${data.image.url} /&gt;
+			&lt;h3&gt;Hello ${data.displayName}&lt;/h3&gt;
+			`);*/
+		})
 });
 
 app.use("/", function (req, res) {
-    var url = getAuthUrl();
-    res.send("Authentication using google oAuth!" + "URL:[" + url + "]");
-    //window.open(url,'mywindow','width=400,height=200')
+	var url = getAuthUrl();
+	res.send("Authentication using google oAuth!" + "URL:[" + url + "]");
+	//window.open(url,'mywindow','width=400,height=200')
 });
 
 function listEvents(auth) {
-
-    console.log("Called Listing Event Functions");
-  var calendar = google.calendar('v3');
-  calendar.events.list({
-    auth: auth,
-    calendarId: 'primary',
-    timeMin: (new Date()).toISOString(),
-    maxResults: 10,
-    singleEvents: true,
-    orderBy: 'startTime'
-  }, function(err, response) {
-    if (err) {
-      console.log('The API returned an error: ' + err);
-      return;
-    }
-    var events = response.items;
-    if (events.length == 0) {
-      console.log('No upcoming events found.');
-    } else {
-      console.log('Upcoming 10 events:');
-      for (var i = 0; i < events.length; i++) {
-        var event = events[i];
-        var start = event.start.dateTime || event.start.date;
-        console.log('%s - %s', start, event.summary);
-      }
-    }
-  });
-
-  console.log("Exited Listing Event Functions");
+	var calendar = google.calendar('v3');
+	/*
+	calendar.calendars.get({
+		auth: auth,
+		calendarId: 'en-gb.indian%23holiday%40group.v.calendar.google.com',
+		//timeMin: (new Date("2017-01-01")).toISOString(),
+		//timeMax: (new Date("2017-12-31")).toISOString(),
+		//maxResults: 10,
+		//singleEvents: true,
+		//orderBy: 'startTime'
+	}, function(err, response) {
+		console.log("Function calledback");
+		if (err) {
+			console.log('The API returned an error: ' + err);
+			return;
+		}
+		console.log(JSON.stringify(response));
+	});
+	*/
+	calendar.events.list({
+		auth: auth,
+		calendarId: 'en-gb.indian%23holiday%40group.v.calendar.google.com',
+		timeMin: (new Date("2017-01-01")).toISOString(),
+		timeMax: (new Date("2017-12-31")).toISOString(),
+		//maxResults: 10,
+		//singleEvents: true,
+		//orderBy: 'startTime'
+	}, function(err, response) {
+		if (err) {
+			console.log('The API returned an error: ' + err);
+			return;
+		}
+		var events = response.items;
+		if (events.length == 0) {
+			console.log('No upcoming events found.');
+		} else {
+			console.log("Upcoming [" +  events.length + "] events");
+			for (var i = 0; i < events.length; i++) {
+				var event = events[i];
+				var start = event.start.dateTime || event.start.date;
+				console.log('%s - %s', start, event.summary);
+			}
+		}
+	});
 }
 
 
 var port = 8080;
 var server = http.createServer(app);
 server.listen(port);
-server.on('listening', function () {Exited
-    console.log("listening to "+ port);
+server.on('listening', function () {
+	console.log("listening to "+ port);
 });
