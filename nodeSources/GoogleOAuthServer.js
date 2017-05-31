af36e39f78f74dea0c365769cbcbbe1016a8edaf
Rakesh Gajjar <gajjarrakesh@users.noreply.github.com>
2017-05-31 18:42:03 +0530

    Improvements
    1. Date diff calculation
    2.Day no. addition 
    3. Launching browser to redirect user to permission/login page

--- nodeSources/GoogleOAuthServer.js
+++ nodeSources/GoogleOAuthServer.js
@@ -2,11 +2,41 @@ var http = require('http');
 var express = require('express');
 var Session = require('express-session');
 var google = require('googleapis');
+//var opn = require('opn');
+var openUrl = require('openurl');
+
 var plus = google.plus('v1');
 var OAuth2 = google.auth.OAuth2;
+
+
 const ClientId = "947156970260-36nhr0l2nbvlbl723m5ehjolubi9vnub.apps.googleusercontent.com";
 const ClientSecret = "S0c4a5YlTENZvHtLrUqO7iCo";
 const RedirectionUrl = "http://localhost:8080/oauthCallback";
+
+var weekday = new Array(7);
+weekday[0] =  "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+function getDaysBetweenDates(d0, d1) {
+
+  var msPerDay = 8.64e7;
+
+  // Copy dates so don't mess them up
+  var x0 = new Date(d0);
+  var x1 = new Date(d1);
+
+  // Set to noon - avoid DST errors
+  x0.setHours(12,0,0);
+  x1.setHours(12,0,0);
+
+  // Round to remove daylight saving errors
+  return Math.round( (x1 - x0) / msPerDay );
+}
 
 var app = express();
 app.use(Session({
@@ -48,7 +78,7 @@ app.use("/oauthCallback", function (req, res) {
 	  	res.send("Login successful!!" + "Token:[" + JSON.stringify(tokens) + "]"+ " Sessions:[" + JSON.stringify(session) + "]");
 		//&lt;a href="/details"&gt;Go to details page&lt;/a&gt;      
 
-		console.log("Tokens Sending to list Function:" + JSON.stringify(session["tokens"]));
+		//console.log("Tokens Sending to list Function:" + JSON.stringify(session["tokens"]));
 		listEvents(oauth2Client);
 	}
 	else{
@@ -75,12 +105,15 @@ app.use("/details", function (req, res) {
 			&lt;img src=${data.image.url} /&gt;
 			&lt;h3&gt;Hello ${data.displayName}&lt;/h3&gt;
 			`);*/
+		console.log("Hello " + data.displayName);
 		})
 });
 
 app.use("/", function (req, res) {
 	var url = getAuthUrl();
 	res.send("Authentication using google oAuth!" + "URL:[" + url + "]");
+	openUrl.open(url);
+	//opn(url, {app:'chrome'});
 	//window.open(url,'mywindow','width=400,height=200')
 });
 
@@ -117,15 +150,27 @@ function listEvents(auth) {
 			console.log('The API returned an error: ' + err);
 			return;
 		}
+		var start;
+		var prev;
 		var events = response.items;
 		if (events.length == 0) {
-			console.log('No upcoming events found.');
+			console.log('No Holidays found.');
 		} else {
-			console.log("Upcoming [" +  events.length + "] events");
+			console.log("Upcoming [" +  events.length + "] Holidays");
 			for (var i = 0; i < events.length; i++) {
+
 				var event = events[i];
+				prev = start;
 				var start = event.start.dateTime || event.start.date;
-				console.log('%s - %s', start, event.summary);
+
+				//Check to remove all Sundays and Saturdays for computing leave list
+				//if(new Date(start).getDay() == 0 || new Date(start).getDay() == 6 ){
+				//	continue;
+				//}
+				//else
+				{ 
+					console.log('%s [%s] Diff[%d] - %s', start, weekday[new Date(start).getDay()],getDaysBetweenDates(new Date(prev), new Date(start)), event.summary);				
+				}
 			}
 		}
 	});
