402dd74ca24bca567386a34149e1ad7d061d4b20
Rakesh Gajjar <gajjarrakesh@users.noreply.github.com>
2017-06-29 17:51:34 +0530

    Namespace changes

--- TrieTrees/TrieTreeDriver.cpp
+++ TrieTrees/TrieTreeDriver.cpp
@@ -6,60 +6,83 @@
 #include <string.h>
 #include <sstream>
 #include <time.h>
+#include <limits>
 
 #include "TrieTree.h"
 
-#define SAMPLE_INPUT_SIZE 9999
+#define SAMPLE_INPUT_SIZE 99999
 #define MAX_ARRAY_SIZE 100
 #define ASCII_TABLE_SIZE 128
 #define ASCII_SPACE_CHAR 32
 #define WORD_LENGTH 25
 
+
+#ifndef _USE_GET_LINE_
+#define _USE_GET_LINE_
+#endif
+
 void Test_SampleInput();
 void Test_RandomisedDataInput();
 int Solve();
+
+using namespace Tries;
 
 TrieTree* pInstance =NULL;
 
 void Test_SampleInput(){
 
-	pInstance  = TrieTree::GetInstance();
-	if ( !pInstance ){
-		cout << "TrieTree failed!" << endl;
-		return;
-	}
-	char A[MAX_ARRAY_SIZE] = { 0 };	
-
 	int test_cases = 0;
 	int size= 0;
 
 	std::string inputString;
+	char A[MAX_ARRAY_SIZE] = { 0 };	
 
+	pInstance  = TrieTree::GetInstance();
+	if ( !pInstance ){  cout << "TrieTree failed!" << endl; return ; }
+	
 	freopen("TrieTree_Sample_Input.txt", "r", stdin);
 
+	
+#ifdef _USE_GET_LINE_
 	//Converting String to Number using getline
 	getline(cin, inputString);
 
+	cout << "getline input strings:[" << inputString << "]" << endl;
 	// This code converts from string to number safely.
 	stringstream myStream(inputString);
+
 	if ( myStream >> test_cases ){
 		cout << "Test cases :[" << test_cases << "]" <<  endl;
 	} else {
 		cout << "Invalid number, please try again" << endl;
+		return;
 	}
 
 	for (int tc = 0; tc < test_cases; tc++){
 		
 		getline(cin , inputString);
-		pInstance->AddWord((char*)inputString.c_str());
+		cout << "Read String:[" << inputString << "]" << endl;
+		pInstance->AddWord(inputString);
 	}
-
+	int Ans = Solve();	
+	inputString.clear();
+	cout << "Last CIN String:[" << inputString << "]" << endl;
+	fclose(stdin);
+#else 
+	std::cin >> test_cases;
+	cout << "Read TCs:[" << test_cases << "]" << endl;
+	while(std::cin>>inputString)  {
+		cout << "Read String:[" << inputString << "]" << endl;
+		pInstance->AddWord(inputString);
+	}
 	int Ans = Solve();
-	/*std::string searchString;
-	while((std::cin>>searchString))  {
-		cout << "Read String:[" << searchString << "]" << endl;
-	}*/
+	std::cin >> inputString;
+
+#endif
+
 }
+
+
 void Test_RandomisedDataInput(){
 
 	pInstance = TrieTree::GetInstance();
@@ -117,6 +140,7 @@ int Solve(){
 int main(int argc, char* argv[]){
 
 	cout << "Arg Count:" << argc << endl;
+	
 	if (argc > 1){
 		cout << "Arg :" << argv[1] << endl;
 		if (strcmp(argv[1], "0") == 0){
