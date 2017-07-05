f1f4bc4c6e02964479919723d093e9ce0d2278fc
Rakesh Gajjar <gajjarrakesh@users.noreply.github.com>
2017-07-05 09:35:09 +0530

    Replacing pointers with smart pointers
    This change will eliminate the need of freeing memory and would help resolving issue of memory leaks and memory management.

--- TrieTrees/TrieNode.h
+++ TrieTrees/TrieNode.h
@@ -4,21 +4,24 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string.h>
+#include <memory>
 
 using namespace std;
 
 namespace Tries{
 
+	static long long int nodeCounter;
+
 	static const char TRIE_EMPTY_CHAR='\0';
-	static const int  TRIE_MAX_SIZE=128; //26	//All character set representable by ASCII character set	
+	static const int  TRIE_MAX_SIZE=128; 			//26	//All character set representable by ASCII character set	
 	static const int  TRIE_START_CHAR=32; 			//Beginning of the meaning full charactes 
 
 	class ITrieTreeNode;
 	class TrieTreeNode;
 	class TrieNodeFactory;
 
-	typedef ITrieTreeNode ITrieNodeInterface;
-	typedef ITrieTreeNode* ITrieNodeInterfacePtr;
+	typedef ITrieTreeNode 	ITrieNodeInterface;
+	typedef ITrieTreeNode* 	ITrieNodeInterfacePtr;
 	typedef ITrieTreeNode** ITrieNodeInterfaceDPtr;
 
 	typedef TrieTreeNode  	TTNode;
@@ -29,12 +32,16 @@ namespace Tries{
 	typedef TrieNodeFactory*  TTNFPtr;
 	typedef TrieNodeFactory** TTNFDPtr;
 
+	typedef std::shared_ptr<ITrieTreeNode> ITrieNodeIntSharedPtr;
+
 	class ITrieTreeNode{
 	protected:
+		//int nodeCounter;
 		bool	bHasChild;
 		bool	bEndOfWord;
 		char	charValue;
-		ITrieNodeInterfacePtr	pChild[TRIE_MAX_SIZE];
+		//ITrieNodeInterfacePtr	pChild[TRIE_MAX_SIZE];
+		ITrieNodeIntSharedPtr 	pChild[TRIE_MAX_SIZE];
 	public:
 		/*ITrieTreeNode();
 		virtual ~ITrieTreeNode();*/
@@ -45,8 +52,8 @@ namespace Tries{
 		virtual char GetCharValue() = 0;
 		virtual void SetCharValue(char value) = 0;
 		virtual void ResetChildren() = 0;
-		virtual ITrieNodeInterfacePtr GetChildPtr(int index) = 0;
-		virtual void SetChildPtr(ITrieNodeInterfacePtr pChildPtr, int index) = 0;
+		virtual ITrieNodeIntSharedPtr GetChildPtr(int index) = 0;
+		virtual void SetChildPtr(ITrieNodeIntSharedPtr pChildPtr, int index) = 0;
 	};
 
 	class TrieTreeNode : public ITrieTreeNode{
@@ -62,23 +69,26 @@ namespace Tries{
 		char GetCharValue();
 		void SetCharValue(char value);
 		void ResetChildren();
-		ITrieNodeInterfacePtr  GetChildPtr(int index);
-		void SetChildPtr(ITrieNodeInterfacePtr pChildPtr, int index);
+		ITrieNodeIntSharedPtr  GetChildPtr(int index);
+		void SetChildPtr(ITrieNodeIntSharedPtr pChildPtr, int index);
 	};
 
 	class TrieNodeFactory{
 	public:
 		static TTNFPtr mInstance;
 	private:
-		int nodeCounter;
+		//int nodeCounter;
 		TrieNodeFactory();
 	public:
 		~TrieNodeFactory();
 		static TTNFPtr GetInstance();
 		static void CleanUp(void);
-		TTNodePtr CreateTrieNode();
+		/*TTNodePtr*/ITrieNodeIntSharedPtr CreateTrieNode();
 		int GetNodeCount(void);
 	};
+
+
+	typedef std::shared_ptr<TTNode> TTNodeSharedPtr;
 
 }
 #endif //#ifndef __TRIE_NODE_H__
