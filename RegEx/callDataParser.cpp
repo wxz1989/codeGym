19288cef83621180d46cbc5c112d58f7d6c67bb6
lifelongstudent <rakeshgajjar1983@gmail.com>
2017-07-16 00:16:41 +0530

    Parsing Duration and Balance Values

--- RegEx/callDataParser.cpp
+++ RegEx/callDataParser.cpp
@@ -22,19 +22,31 @@ void IterativeMatching(std::string inputString){
 	std::smatch m;
 	//std::regex e ("\\b(sub)([^ ]*)");   // matches words beginning by "sub"
 	std::regex cost("(.?chrg).*([0-9])|(.?call.*cost).*([0-9])|(.?dur.*).*([0-9])|(.?bal.*).*([0-9])", ECMAScript | icase);
-	std::regex duration("(.?dur.*(?:(?:[0-9]+:[0-9]+)|(?:[0-9]+(?:.)?(?:se(?:c|cond|conds|cs)|mi(?:ns|nutes)|h(?:our||hr)))))", ECMAScript | icase);
+	//std::regex duration("(.?dur.*(?:(?:[0-9]+:[0-9]+)|(?:[0-9]+(?:.)?(?:se(?:c|cond|conds|cs)|mi(?:ns|nutes)|h(?:our||hr)))))", ECMAScript | icase);
+	std::regex duration("(.?dur.*(?:(?:(?:.)?[0-9]+(?:.)?:(?:.)?[0-9]+(?:.)?)|(?:(?:.)?[0-9]+(?:.)?(?:se(?:c|cond|conds|cs)|mi(?:ns|nutes)|h(?:our||hr)))))", ECMAScript|icase);
 	std::regex balance(".?bal.*(?:Rs)?.*[0-9]+\\.[0-9]{0,5}(?:inr|INR|.{0,1})?", ECMAScript | icase);
 	std::cout << "Target sequence: " << inputString << std::endl;
 	std::string durationNumbers="Duration:00:00:08";
-	std::regex numbers("[0-9:]+", ECMAScript | icase);
+	std::regex durRegExe("[0-9:]+", ECMAScript | icase);
+	std::regex balanceValues("[0-9]+(?:\\.[0-9]+|\\.)", ECMAScript | icase);
 
 	int matchCount = 0;
 	cout << "Match begins" << endl;
-	std::regex_search ( durationNumbers, m, numbers );
+	std::regex_search ( inputString, m, balance );
 
   	std::cout << "matches:" << std::endl;
   	for (std::smatch::iterator it = m.begin(); it!=m.end(); ++it) {
 		std::cout << *it << std::endl;
+
+		std::string numSearch = *it;
+
+		std::smatch numMatches;
+		cout << "Digit Match begins" << endl;
+		std::regex_search ( numSearch, numMatches, balanceValues );
+		for (std::smatch::iterator ite= numMatches.begin(); ite!=numMatches.end(); ++ite) {
+			std::cout << *ite << std::endl;
+  		}
+  		cout << "Digit Match ends" << endl;
   	}
   	cout << "Match ends" << endl;
 }
@@ -79,7 +91,7 @@ int main ()
 	std::string operatorString[SUPORTED_OPERATORS] = { "Callcost:Rs.0.20 Duration:00:00:08  Bal:Rs.157.31 Govt. mandates to link your Aadhar with mobile number. Visit Retailer",
 									"CallCost:Rs.0.04 Duration:00:00:02 Bal:Rs.82.29 Now share your 3G/4G pack with loved ones-SMS ADD <10 Digit Mob No> to 121",
 									"CHRG:0.300INR,DURN:003Sec,BAL-LEFT:31.746INR..RC24-1p/sec Local/STD for 28 days",
-									"Your last call cost Rs.0.400, call duration 0:0 : 4, Main A / C balance Rs.194.485 ** For web SELFCARE please visit www.bsnlszprepaid.com **",
+									"Your last call cost Rs.0.400, call duration 0:0:4, Main A / C balance Rs.194.485 ** For web SELFCARE please visit www.bsnlszprepaid.com **",
 									"CallCostRs0.8 BallRs16.83 Duration3sec Spl 23: Local Calls at 25p / min for 28 days"
 									};
 	std::string s("Callcost:Rs.0.20 Duration:00:00:08  Bal:Rs.157.31 Govt. mandates to link your Aadhar with mobile number. Visit Retailer");
