95278b2ae60cc10eb0bd4efba45d0debdb8efa8e
lifelongstudent <rakeshgajjar1983@gmail.com>
2017-07-17 08:37:56 +0530

    Completed Parsing

--- RegEx/callDataParser.cpp
+++ RegEx/callDataParser.cpp
@@ -5,6 +5,7 @@
 #include <algorithm> 
 #include <cctype>
 #include <locale>
+#include <stdlib.h>
 
 #define SUPORTED_OPERATORS 5
 
@@ -18,47 +19,93 @@ static inline void ltrim(std::string &s) {
 	}));
 }
 
-void IterativeMatching(std::string inputString){
+
+enum ParsingCategory{
+	CALL_COST=0,
+	CALL_DURATION,
+	REM_BALANCE,
+	INVALID
+};
+
+int GetSeconds(std::string strDuration){
+
+	cout << strDuration.c_str() << endl;
 	std::smatch m;
-	//std::regex e ("\\b(sub)([^ ]*)");   // matches words beginning by "sub"
+	std::regex durSeconds("[0-9]+");
+
+	std::regex_search ( strDuration, m, durSeconds);
+	for (std::smatch::iterator it = m.begin(); it!=m.end(); ++it) {
+		std::cout << *it ;
+	}
+	return 0;
+}
+
+void CallDataParser(std::string inputString, ParsingCategory parseCategory){
+	std::smatch m;
 	std::regex cost("(?:.?cost.?(?:Rs.|Rs|:|\\.)?[0-9]+(?:\\.[0-9]+)?)|(?:.?chrg.?(?:Rs.|Rs|:|\\.)?[0-9]+(?:\\.[0-9]+)?)", ECMAScript | icase);
-	//std::regex duration("(.?dur.*(?:(?:[0-9]+:[0-9]+)|(?:[0-9]+(?:.)?(?:se(?:c|cond|conds|cs)|mi(?:ns|nutes)|h(?:our||hr)))))", ECMAScript | icase);
 	std::regex duration("(?:.?dur.*(?:(?:(?:.)?[0-9]+(?:.)?:(?:.)?[0-9]+(?:.)?)|(?:(?:.)?[0-9]+(?:.)?(?:se(?:c|cond|conds|cs)|mi(?:ns|nutes)|h(?:our||hr)))))", ECMAScript|icase);
 	std::regex balance(".?bal.*(?:Rs)?.*[0-9]+\\.[0-9]{0,5}(?:inr|INR|.{0,1})?", ECMAScript | icase);
 	std::cout << "Target sequence: " << inputString << std::endl;
-	std::regex durValRegex("(?:[0-9](?::)?(?:.)*)+");
+	std::regex durValRegex("(?:[0-9](?::)?(?::| )*)+");
 	std::regex CostAndBalValRegEx("[0-9]+(?:\\.[0-9]+|\\.)");
 	std::regex durUnitRegEx("sec|min|hours|hr", ECMAScript|icase);
 
 	int matchCount = 0;
 	cout << "Match begins" << endl;
-	std::regex_search ( inputString, m, duration );
 
-  	std::cout << "matches:" << std::endl;
-  	for (std::smatch::iterator it = m.begin(); it!=m.end(); ++it) {
-		std::cout << *it << std::endl;
+	switch(parseCategory){
+		case CALL_COST:{
+		case REM_BALANCE:{
+				std::regex_search ( inputString, m, (parseCategory==CALL_COST ?  cost : balance));
+				std::cout << "Matches: [ ";
+				for (std::smatch::iterator it = m.begin(); it!=m.end(); ++it) {
+					std::cout << *it << "]";
 
-		std::string numSearch = *it;
+					std::string numSearch = *it;
 
-		std::smatch numMatches;
-		cout << "Digit Match begins" << endl;
-		std::regex_search ( numSearch, numMatches, durValRegex );
-		for (std::smatch::iterator ite= numMatches.begin(); ite!=numMatches.end(); ++ite) {
-			std::cout << *ite << std::endl;
-
-			std::string unitSearch = *ite;
-
-			std::smatch unitMatch;
-			cout << "Unit Match begins" << endl;
-			std::regex_search ( unitSearch, unitMatch, durUnitRegEx );
-			for (std::smatch::iterator iteU= unitMatch.begin(); iteU!=unitMatch.end(); ++iteU) {
-				std::cout << *iteU << std::endl;
+					std::smatch numMatches;
+					cout << "Digits:[";
+					std::regex_search ( numSearch, numMatches, CostAndBalValRegEx );
+					for (std::smatch::iterator ite= numMatches.begin(); ite!=numMatches.end(); ++ite) {
+						std::cout << *ite <<"]";
+					}
+				}
 			}
-			cout << "Unit Match ends" << endl;
-  		}
-  		cout << "Digit Match ends" << endl;
-  	}
-  	cout << "Match ends" << endl;
+		}
+		break;
+		case CALL_DURATION:{
+			std::regex_search ( inputString, m, duration );
+			std::cout << "Matches:[" ; 
+		  	for (std::smatch::iterator it = m.begin(); it!=m.end(); ++it) {
+				std::cout << *it << "]" ;
+
+				std::string numSearch = *it;
+
+				std::smatch numMatches;
+				cout << "Digit:[";
+				std::regex_search ( numSearch, numMatches, durValRegex );
+				for (std::smatch::iterator ite= numMatches.begin(); ite!=numMatches.end(); ++ite) {
+					std::cout << *ite << "]";
+					
+					GetSeconds(*ite);
+
+					std::string unitSearch = *ite;
+
+					std::smatch unitMatch;
+					cout << "Unit:[ ";
+					std::regex_search ( numSearch, unitMatch, durUnitRegEx );
+					for (std::smatch::iterator iteU= unitMatch.begin(); iteU!=unitMatch.end(); ++iteU) {
+						std::cout << *iteU << "]";
+					}
+		  		}
+		  	}
+		}
+		break;
+		default:{
+
+		}
+	}
+  	cout << "]" << endl;
 }
 
 void TestParse(std::string inputString, const std::regex& e){
@@ -95,7 +142,7 @@ void TestParse(std::string inputString, const std::regex& e){
 	cout << "Match ends" << endl;
 }
 
-int main ()
+int main (int argc, char* argv[])
 {
 	
 	std::string operatorString[SUPORTED_OPERATORS] = { "Callcost:Rs.0.20 Duration:00:00:08  Bal:Rs.157.31 Govt. mandates to link your Aadhar with mobile number. Visit Retailer",
@@ -104,12 +151,12 @@ int main ()
 									"Your last call cost Rs.0.400, call duration 0:0:4, Main A / C balance Rs.194.485 ** For web SELFCARE please visit www.bsnlszprepaid.com **",
 									"CallCostRs0.8 BallRs16.83 Duration3sec Spl 23: Local Calls at 25p / min for 28 days"
 									};
-	std::string s("Callcost:Rs.0.20 Duration:00:00:08  Bal:Rs.157.31 Govt. mandates to link your Aadhar with mobile number. Visit Retailer");
-	std::regex e ("\\b(sub)([^ ]*)");   // matches words beginning by "sub"
-
 	for (int i = 0; i < SUPORTED_OPERATORS; i++){
 		//TestParse(operatorString[i], e);
-		IterativeMatching(operatorString[i]);
+		int argument = atoi(argv[1]);
+		if ( argument <=0 || argument > 2){ argument = 0; }
+
+		CallDataParser(operatorString[i], (ParsingCategory) argument);
 	}
 	
 	return 0;
