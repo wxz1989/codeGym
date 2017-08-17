f0f36e463c7276fafcb7c6d685013fd1da4ee2a0
Rakesh Gajjar <gajjarrakesh@users.noreply.github.com>
2017-08-17 19:12:36 +0530

    Incremental changes and Stabalization

--- TrieTrees/TrieTree.h
+++ TrieTrees/TrieTree.h
@@ -6,6 +6,8 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include <fstream>
 
 #include "TrieNode.h"
 
@@ -20,18 +22,24 @@ typedef TrieTree** TTDPtr;
 
 typedef std::shared_ptr<TrieTree> TTSharedPtr;
 
+
 class TrieTree{
 
+	const std::string _TRIE_DIRECTORY_FILE_NAME_ = ".trie_words_directory.txt";
 public:
 	bool	AddWord(const std::string& inputString);
-	void	FindWord(const std::string& inputString);
+	bool	FindWord(const std::string& inputString);
+	void	ListAll(void);
 	int		FindWordByPrefix(const std::string& inputText);
 	bool 	DeleteWord(const std::string& inputWord);
 private:
 
+	bool 	DeleteWordUtil(ITrieNodeIntSharedPtr pHead, std::string inputText, int currentIndex);
+	void AddToDirectory(const std::string&);
 	int FindCharIndex(char currentChar);
 	/*TTNodePtr*/ ITrieNodeIntSharedPtr FindNewHead(const std::string&);
 	int	ExtractWordFromNode(/*TTNodePtr*/ITrieNodeIntSharedPtr pHead, const std::string&, std::string&);
+	int ExtractWordsFromNode(ITrieNodeIntSharedPtr pHead, std::string& outputText);
 #if 0 
 	TTNodePtr 	TrieNodeFactory();
 	int	FreeTrieTree(TTNodePtr);
@@ -45,6 +53,7 @@ public:
 	static TTPtr 	GetInstance();
 	static void TrieTerminate();
 private:
+	//File* m_DirHandle;
 	static TTPtr pInstance;
 	/*TTNodePtr*/ ITrieNodeIntSharedPtr pTreeHead;
 	//TTNFPtr	nodeFactoryPtr;			//No Specific need of keeping this as member
