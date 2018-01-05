42226b8402b6a23b6788867538e8ac7be1efb86d
Rakesh Gajjar <gajjarrakesh@users.noreply.github.com>
2018-01-05 11:33:24 +0000

    Added Balancing Working Logic
    1. Added logic to balance the unbalanced/skewed tree to a balanced tree
    Algo: 
    Step-1: Take InOrder traversal of the BST and store it in an array, make low =0 High = Length
    Step-2:  Run a loop to find the middle (low + high)/2 using Divide and Conquer approach of the array and make that a new HEAD of the tree, 
    Step-3: Insert the middle node to the new Tree.
    Step-4:   Divide the inorder traversal array in to LEFT and RIGHT part from the new HEAD and recursively follow steps 2.
    Step-5: Print new Tree and check each node's left and right node count(you will find them balanced)

--- Tree-Graphs/BalancingTree.cpp
+++ Tree-Graphs/BalancingTree.cpp
@@ -5,7 +5,9 @@
 //#include <bits/stdc++.h>
 
 #define MAX_NODES 100
+int index =0;
 int ar[MAX_NODES] ={0};
+int balanced[MAX_NODES] ={0};
 
 using namespace std;
 
@@ -16,22 +18,40 @@ struct Node{
 	Node* pLeft,*pRight;
 };
 
+typedef struct Node Node;
 typedef struct Node* NodePtr;
 NodePtr  _pRoot = nullptr;
 
 NodePtr CreateNode(int value){
-	NodePtr pTemp = new struct Node;
+	NodePtr pTemp = new Node();
 	pTemp->height = pTemp->lC = pTemp->rC = 0;
 	pTemp->pLeft = pTemp->pRight = nullptr;
 	pTemp->data = value;
 	return pTemp;
 }
 
+void ReleaseUtil(NodePtr pR){
+
+	if(pR == nullptr){ return; }
+	ReleaseUtil(pR->pLeft);
+	ReleaseUtil(pR->pRight);
+	//cout << "Delete:["<< pR->data << "]" << endl; 
+	pR->pLeft = nullptr;
+	pR->pRight = nullptr;
+	delete pR;
+}
+
+void Release(NodePtr pR){
+
+	if(pR == nullptr){ return; }
+	ReleaseUtil(pR);
+	_pRoot = nullptr;
+}
+
 void Insert(NodePtr pR,int value){
 
 	if( _pRoot == nullptr){ 
-		pR = CreateNode(value);  
-		_pRoot = pR; 
+		_pRoot = CreateNode(value);
 		return;
 	}
 
@@ -53,22 +73,67 @@ void InOrder(NodePtr pR){
 	if(pR == nullptr){ return;  }
 	InOrder(pR->pLeft);
 	cout << pR->data <<"-"<< pR->lC << "-"<<pR->rC <<endl;
+	balanced[index++] = pR->data;
 	//cout << pR->data <<" " ;
 	InOrder(pR->pRight);
 }
 
+void PrintBalancedTree(){
+	cout << "Balanced BST" << endl;
+	for(int i = 0; i< index; i++){
+		cout << balanced[i] << " ";
+	}
+	cout << endl;
+}
+
+void BalanceUtil(NodePtr pR,int low,int high){
+	
+	if(low >= high){ return;  }
+	int mid = (low + high) /2;
+
+	cout << "Bal Inserting:[" << balanced[mid] <<"]" << endl;
+	Insert(_pRoot,balanced[mid]);
+
+	BalanceUtil(pR,low,mid);
+	BalanceUtil(pR,mid+1,high);
+}
+NodePtr Balance(){
+	
+	if(index  == 0){ return nullptr;  }
+	
+	Release(_pRoot);
+
+	int low = 0; 
+	int high = index;
+
+	BalanceUtil(_pRoot,low,high);
+
+	//Clear Balalnced Array and make index 0// Imp for my logic not for the algo
+	memset(balanced,0,sizeof(int)*index);
+	index = 0;
+	// ends 
+
+	InOrder(_pRoot);
+	PrintBalancedTree();
+	return nullptr;
+}
+
 //Sample input 
-//30 20 12 28 60 50 90 120 121 122 123 124 125 126 127 128 129 130
+/*
+18
+30 20 12 28 60 50 90 120 121 122 123 124 125 126 127 128 129 130
+5
+10 20 30 40 50
+5
+50 40 30 20 10
+*/
 
-void Release(NodePtr pR){
+NodePtr InOrderSuc(NodePtr pR,int value){
+	return nullptr;
+}
 
-	if(pR == nullptr){ return; }
-	Release(pR->pLeft);
-	Release(pR->pRight);
-	//cout << "Delete:["<< pR->data << "]" << endl; 
-	pR->pLeft = nullptr;
-	pR->pRight = nullptr;
-	delete pR;
+NodePtr InOrderPred(NodePtr pR,int value){
+	return nullptr;
 }
 
 　
@@ -85,8 +150,10 @@ int main(){
 		}
 		InOrder(_pRoot) ;
 		cout << endl;
+		PrintBalancedTree();
+		Balance();
+
 		Release(_pRoot);
-		_pRoot = nullptr;
 	}
 	_getch();
 	return 0;
