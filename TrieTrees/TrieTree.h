3e0a7779a1e62608c737e4d39fdae1394f5627c5
Rakesh Gajjar <gajjarrakesh@users.noreply.github.com>
2018-01-23 17:03:02 +0530

    Adding Hashing Functionality to Trie
    Hashing Impl. tested with English Dictionary database

--- TrieTrees/TrieTree.h
+++ TrieTrees/TrieTree.h
@@ -25,8 +25,18 @@ typedef std::shared_ptr<TrieTree> TTSharedPtr;
 
 class TrieTree{
 
+	//Hash Computation
+	
+	static const unsigned int MOD_PRIME = 2069;
+	static const int HASH_TABLE_SIZE = 300;
+	int hashElementCount;
+	int CollisionArray[MOD_PRIME];
+	//Hash Table
+
+
 	long mWordCount;
 	const std::string _TRIE_DIRECTORY_FILE_NAME_ = ".trie_words_directory.txt";
+	const std::string _ENGLISH_DICTIONARY_ = ".englishdictionary.txt";
 public:
 	bool	AddWord(const std::string& inputString);
 	bool	FindWord(const std::string& inputString);
@@ -34,14 +44,16 @@ public:
 	int		FindWordByPrefix(const std::string& inputText);
 	bool 	DeleteWord(const std::string& inputWord);
 	bool 	BuildFromDirectory(void);
+	bool 	BuildFromEnglishDirectory(void);
+	void 	ShowCollisionArray();
 private:
-
 	bool 	DeleteWordUtil(ITrieNodeIntSharedPtr pHead, std::string inputText, int currentIndex, bool& wordRemoved);
 	void AddToDirectory(const std::string&);
 	int FindCharIndex(char currentChar);
 	/*TTNodePtr*/ ITrieNodeIntSharedPtr FindNewHead(const std::string&);
 	int	ExtractWordFromNode(/*TTNodePtr*/ITrieNodeIntSharedPtr pHead, const std::string&, std::string&);
 	int ExtractWordsFromNode(ITrieNodeIntSharedPtr pHead, std::string& outputText);
+	int Hash(const std::string& source);
 #if 0 
 	TTNodePtr 	TrieNodeFactory();
 	int	FreeTrieTree(TTNodePtr);
